0fed2a8aabed92ae749c8f6f4c9c3c13f1073006
Jenni <jennimmartin@outlook.com>
2024-07-30 21:02:20 +0200

    remove underline

--- src/components/SectionTitle.jsx
+++ src/components/SectionTitle.jsx
@@ -1,7 +1,7 @@
 const SectionTitle = ({ text }) => {
   return (
     <div className="p-5">
-      <h2 className="text-center text-pink-500 font-bold text-xl capitalize underline underline-offset-4 py-3">
+      <h2 className="text-center text-pink-500 font-bold text-xl capitalize py-3">
         {text}
       </h2>
     </div>
